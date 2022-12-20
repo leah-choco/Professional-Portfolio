ee99e8ddd5830c6d8d74e229c84973105296aad2
Your Name <you@example.com>
2022-12-20 13:28:33 -0600

    Writing about me page and adding CSS class

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -4,3 +4,19 @@ import "../../assets/About.css";
 
 //My photo
 //Component with short life story
+
+export default function About() {
+  return (
+    <div>
+      <h1>Leah Choco</h1>
+      <img></img>
+      <p className="about-me">
+        I currently reside in Middle Tennessee. Juan, my husband, and I are
+        raising three children. We recently decided that I should complete a
+        coding bootcamp to be certified in MERN. I am incredibly motivated to
+        continue my education. This is the place to find links to my projects,
+        and to all the other resources showing my work and experience.
+      </p>
+    </div>
+  );
+}
