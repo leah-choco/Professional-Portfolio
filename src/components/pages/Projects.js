72bf4d747125715a5046f4bd469e27c2362b52fe
Your Name <you@example.com>
2022-12-30 14:45:46 -0600

    Working on adding flexbox syntax to get the projects rendering side by side

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -49,38 +49,70 @@ export default Projects;*/
 
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Image from "../../assets/images/dave-ruck-GQZg8CrP3sc-unsplash.jpg";
+import BoatImage from "../../assets/images/dave-ruck-GQZg8CrP3sc-unsplash.jpg";
+import ComingSoon from "../../assets/images/coming soon.jpeg";
 
 function Projects() {
   return (
-    <Card
-      border="warning"
-      className="project-component"
-      style={{ width: "25rem" }}
-    >
-      <Card.Img className="boat-image" variant="top" src={Image} />
-      <Card.Body>
-        <Card.Title>Goal-E</Card.Title>
-        <Card.Text>
-          This project was built by four student developers using SQL and
-          Handlebars.
-        </Card.Text>
-        <Button
-          variant="warning"
-          className="buttons"
-          href="https://github.com/leah-choco/Goal-E"
-        >
-          Github
-        </Button>
-        <Button
-          className="buttons"
-          href="https://evening-stream-22967.herokuapp.com/"
-          variant="warning"
-        >
-          Deployed Link
-        </Button>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card
+        border="warning"
+        className="project-component"
+        style={{ width: "25rem" }}
+      >
+        <Card.Img className="boat-image" variant="top" src={BoatImage} />
+        <Card.Body>
+          <Card.Title>Goal-E</Card.Title>
+          <Card.Text>
+            This project was built by four student developers using SQL and
+            Handlebars.
+          </Card.Text>
+          <Button
+            variant="warning"
+            className="buttons"
+            href="https://github.com/leah-choco/Goal-E"
+          >
+            Github
+          </Button>
+          <Button
+            className="buttons"
+            href="https://evening-stream-22967.herokuapp.com/"
+            variant="warning"
+          >
+            Deployed Link
+          </Button>
+        </Card.Body>
+      </Card>
+
+      <Card
+        border="warning"
+        className="project-component"
+        style={{ width: "25rem" }}
+      >
+        <Card.Img className="boat-image" variant="top" src={ComingSoon} />
+        <Card.Body>
+          <Card.Title>Chef Rosetta</Card.Title>
+          <Card.Text>
+            This project was built by four student developers using third party
+            APIs and javascript.
+          </Card.Text>
+          <Button
+            variant="warning"
+            className="buttons"
+            href="https://github.com/leah-choco/Goal-E"
+          >
+            Github
+          </Button>
+          <Button
+            className="buttons"
+            href="https://evening-stream-22967.herokuapp.com/"
+            variant="warning"
+          >
+            Deployed Link
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
